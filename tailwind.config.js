17dbaebc6b71db34dd1aaa4df89520afe246c943
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-03-06 11:17:11 +0100

    feat: Create login page + update tailwind config with theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,35 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true
   },
-  purge: [],
-  theme: {
-    extend: {}
+  purge: {
+    content: ['./src/**/*.js', './src/**/**/*.js']
   },
-  variants: {},
-  plugins: []
+  theme: {
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
+  },
+  variants: {
+    extend: {
+      display: ['group-hover']
+    }
+  }
 };
