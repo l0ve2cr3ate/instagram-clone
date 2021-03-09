da0429ec2ae94179c52538fe052959ffb044dfb5
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-03-09 19:52:41 +0100

    feat: Create header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import { lazy, Suspense, FC } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
+import UserContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
 
 const Login: FC = lazy(() => import('./pages/login'));
 const SignUp: FC = lazy(() => import('./pages/sign-up'));
+const NotFound: FC = lazy(() => import('./pages/not-found'));
+const Dashboard: FC = lazy(() => import('./pages/dashboard'));
 
-function App() {
+const App: FC = () => {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<p>...Loading</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>...Loading</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} exact />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
-}
+};
 
 export default App;
