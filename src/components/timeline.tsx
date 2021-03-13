09c8ed6158b5675796d78428a723135fc10bcb1f
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-03-13 19:37:08 +0100

    feat: Create timeline.

---
+++ src/components/timeline.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-nested-ternary */
+import { FC } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+import Post from './post';
+
+const Timeline: FC = () => {
+  const { photos } = usePhotos();
+  return (
+    <div className="container col-span-2">
+      {!photos ? (
+        <>
+          {[...new Array(4)].map((_, index) => (
+            <Skeleton key={index} count={1} width={640} height={500} className="mb-5" />
+          ))}
+        </>
+      ) : photos?.length > 0 ? (
+        photos.map((content) => <Post key={content.docId} content={content} />)
+      ) : (
+        <p className="text text-center text-2xl">Follow people to see photos</p>
+      )}
+    </div>
+  );
+};
+
+export default Timeline;
