f36d6ff7f0fdf43892111a399d44654229b6d4bb
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-03-18 20:04:06 +0100

    feat: Create comments component.

---
+++ src/components/post/comments.tsx
@@ -0,0 +1,50 @@
+import { FC, useState } from 'react';
+import { formatDistance } from 'date-fns';
+import { Link } from 'react-router-dom';
+import AddComment from './add-comment';
+
+export type Comment = {
+  displayName: string;
+  comment: string;
+};
+
+export type CommentsProps = {
+  docId: string;
+  comments: Comment[];
+  posted: number;
+  commentInput: React.RefObject<HTMLInputElement>;
+};
+
+const Comments: FC<CommentsProps> = ({ docId, comments: allComments, posted, commentInput }) => {
+  const [comments, setComments] = useState(allComments);
+
+  return (
+    <>
+      <div className="p-4 pt-1 pb-4">
+        {comments.length >= 3 && (
+          <p className="text-sm text-gray-base mb-1 cursor-pointer">View all comments</p>
+        )}
+
+        {comments.slice(0, 3).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
+        <p className="text-gray-base uppercase text-xs mb-2">
+          {formatDistance(posted, new Date())} ago
+        </p>
+      </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
+    </>
+  );
+};
+
+export default Comments;
