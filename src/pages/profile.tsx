18e98866a97a14aa0570603c7ccd9ebaf7c6bb90
l0ve2cr3ate <l0ve2cr3ate@gmail.com>
2021-03-25 20:47:57 +0100

    feat: Create profile page.

---
+++ src/pages/profile.tsx
@@ -0,0 +1,37 @@
+import { FC, useState, useEffect } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { getUserByUsername } from '../services/firebase';
+import * as ROUTES from '../constants/routes';
+import { User } from '../hooks/use-user';
+import Header from '../components/header';
+import UserProfile from '../components/profile';
+
+const Profile: FC = () => {
+  const { username } = useParams<{ username: string }>();
+  const [user, setUser] = useState<User | null>(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    async function checkUserExists() {
+      const [user] = await getUserByUsername(username);
+      if (user?.userId) {
+        setUser(user);
+      } else {
+        history.push(ROUTES.NOT_FOUND);
+      }
+    }
+
+    checkUserExists();
+  }, [username, history]);
+
+  return user?.username ? (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="mx-auto max-w-screen-lg">
+        <UserProfile user={user} />
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Profile;
